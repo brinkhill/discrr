80c54d711fcd35d78dd4382a54369feebcb6de3c
Brinkley Hill <8brink@gmail.com>
2023-05-22 00:24:45 -0400

    change profile svg

--- app/components/auth-button/SignInButton.jsx
+++ app/components/auth-button/SignInButton.jsx
@@ -28,6 +28,7 @@ const SignInButton = () => {
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
+                className="profile-icon"
               >
                 <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
                 <circle cx="12" cy="7" r="4"></circle>
